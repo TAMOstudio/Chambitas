a5db59abe3f31a97936fd8690ad3ef2b4f86e089
pevargasg <wearetamo@gmail.com>
2019-02-22 23:01:15 -0800

    navbar finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,37 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { SidenavComponent } from "./components/shared/sidenav/sidenav.component";
+
+/* Angular Material */
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    redirectTo: "",
+    pathMatch: "full",
+    component: NavbarComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavbarComponent, SidenavComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/shared/sidenav/sidenav.component.ts
+++ src/app/components/shared/sidenav/sidenav.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  selector: "app-sidenav",
+  templateUrl: "./sidenav.component.html",
+  styleUrls: ["./sidenav.component.scss"]
 })
 export class SidenavComponent implements OnInit {
+  openSidebar = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
