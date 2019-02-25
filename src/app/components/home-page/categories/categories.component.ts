c45486c8744412799916b21c6dce8a5ea18b4485
pevargasg <wearetamo@gmail.com>
2019-02-24 23:29:01 -0800

    categories done

--- src/app/components/home-page/categories/categories.component.ts
+++ src/app/components/home-page/categories/categories.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Categories } from "../../shared/models/categories.model";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  selector: "app-categories",
+  templateUrl: "./categories.component.html",
+  styleUrls: ["./categories.component.scss"]
 })
 export class CategoriesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  categories: Categories[];
+  constructor() {
+    this.categories = [
+      {
+        icon: "fas fa-hammer",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-tools",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-pen-fancy",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-user-md",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-balance-scale",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-landmark",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-plane-departure",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      },
+      {
+        icon: "fas fa-headphones",
+        name: "carpinteria",
+        businesses: [],
+        order: 1
+      }
+    ];
   }
 
+  ngOnInit() {}
 }
