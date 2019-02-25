3f41b971ff5ece3b257c711db834913caa4512af
pevargasg <wearetamo@gmail.com>
2019-02-24 19:45:29 -0800

    business model

---
+++ src/app/components/shared/models/bussiness.model.ts
@@ -0,0 +1,17 @@
+import { Reviews } from "./reviews.model";
+
+export interface Business {
+  name: string;
+  logo: string;
+  imageGallery: string;
+  numberOfEmployees: number;
+  website: string;
+  address: string;
+  yearFounded: string;
+  owner: string;
+  phoneNumber: number;
+  hours: Date[];
+  servicesOffered: string[];
+  businessReviews: Reviews[];
+  userReviews: Reviews[];
+}
