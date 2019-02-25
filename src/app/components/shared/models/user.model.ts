79e21084797680ddc8011a3029f612641c837d79
pevargasg <wearetamo@gmail.com>
2019-02-24 19:45:40 -0800

    user model

---
+++ src/app/components/shared/models/user.model.ts
@@ -0,0 +1,11 @@
+import { Reviews } from "./reviews.model";
+import { Business } from "./bussiness.model";
+
+export interface User {
+  uid: string;
+  email: string;
+  image: string;
+  businessReviews: Reviews[];
+  userReviews: Reviews[];
+  contacts: Business[];
+}
