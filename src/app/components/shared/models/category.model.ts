9c0d9177bb631ad9b3e762eceff816ee94eeba89
pevargasg <wearetamo@gmail.com>
2019-02-26 20:55:12 -0800

    category model updated

---
+++ src/app/components/shared/models/category.model.ts
@@ -0,0 +1,9 @@
+import { Business } from "./bussiness.model";
+
+export interface Category {
+  categoryUid: string;
+  icon: string;
+  name: string;
+  businesses: Business[];
+  order: number;
+}
