3e70a001f623568f15dba0dbfb66d1daa0ed32d6
pevargasg <wearetamo@gmail.com>
2019-02-26 20:55:25 -0800

    category updated

--- src/app/components/home-page/categories/categories.component.ts
+++ src/app/components/home-page/categories/categories.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Categories } from "../../shared/models/categories.model";
+import { Category } from "../../shared/models/category.model";
 
 @Component({
   selector: "app-categories",
@@ -7,52 +7,60 @@ import { Categories } from "../../shared/models/categories.model";
   styleUrls: ["./categories.component.scss"]
 })
 export class CategoriesComponent implements OnInit {
-  categories: Categories[];
+  categories: Category[];
   constructor() {
     this.categories = [
       {
+        categoryUid: "",
         icon: "fas fa-hammer",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-tools",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-pen-fancy",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-user-md",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-balance-scale",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-landmark",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-plane-departure",
         name: "carpinteria",
         businesses: [],
         order: 1
       },
       {
+        categoryUid: "",
         icon: "fas fa-headphones",
         name: "carpinteria",
         businesses: [],
--- src/app/components/shared/models/categories.model.ts
+++
@@ -1,8 +0,0 @@
-import { Business } from "./bussiness.model";
-
-export interface Categories {
-  icon: string;
-  name: string;
-  businesses: Business[];
-  order: number;
-}
