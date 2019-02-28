10a94825c7974d290a6ab431de4f4142a3895bab
pevargasg <wearetamo@gmail.com>
2019-02-27 23:14:07 -0800

    results page with filters

--- src/app/components/results-page/results-page.component.ts
+++ src/app/components/results-page/results-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Business } from "../shared/models/bussiness.model";
 
 @Component({
-  selector: 'app-results-page',
-  templateUrl: './results-page.component.html',
-  styleUrls: ['./results-page.component.scss']
+  selector: "app-results-page",
+  templateUrl: "./results-page.component.html",
+  styleUrls: ["./results-page.component.scss"]
 })
 export class ResultsPageComponent implements OnInit {
-
-  constructor() { }
+  workTypes: string[];
+  selectedWT: string[];
+  selectedStars: number[];
+  results: Business[];
+  constructor() {}
 
   ngOnInit() {
+    this.workTypes = [
+      "Temporal",
+      "Flexible",
+      "Medio Tiempo",
+      "Tiempo Completo"
+    ];
   }
 
+  onNgModelChange(event) {
+    console.log(this.selectedStars);
+  }
 }
