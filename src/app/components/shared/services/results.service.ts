9237b5ddba043ab73de784e075dea96448f038fa
pevargasg <wearetamo@gmail.com>
2019-02-28 01:29:25 -0800

    results service

--- src/app/components/shared/services/results.service.ts
+++ src/app/components/shared/services/results.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "angularfire2/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ResultsService {
+  constructor(private db: AngularFirestore) {}
 
-  constructor() { }
+  getAllResults() {
+    return this.db.collection("business").valueChanges();
+  }
+
+  getResultsByQuery() {
+    return this.db
+      .collection("business", ref => ref.orderBy("reviewsTotal", "desc"))
+      .valueChanges();
+  }
 }
